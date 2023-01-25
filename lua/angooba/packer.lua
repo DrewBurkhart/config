4e31297ae06d58ebd79e7ad97784d65531e14f68
Andrew Burkhart <Burkhartfc@iCloud.com>
2023-01-25 11:09:49 -0700

    Update colorscheme

--- lua/angooba/packer.lua
+++ lua/angooba/packer.lua
@@ -1,10 +1,6 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
     use {
@@ -13,10 +9,10 @@ return require('packer').startup(function(use)
     }
 
     use({
-        'folke/tokyonight.nvim',
-        as = "tokyonight",
+        'rose-pine/neovim',
+        as = "rose-pine",
         config = function()
-            vim.cmd('colorscheme tokyonight')
+            vim.cmd('colorscheme rose-pine')
         end
     })
 
